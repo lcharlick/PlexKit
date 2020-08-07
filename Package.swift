3403c3567d214ccf3e5887cce8b4d8d650f29b16
Lachlan Charlick <lachlan.charlick@gmail.com>
2020-08-07 14:39:11 +0930

    SPM fixes

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -25,10 +25,7 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "PlexKitTests",
-            dependencies: ["PlexKit"],
-            resources: [
-                .process("Resources")
-            ]
+            dependencies: ["PlexKit"]
         ),
     ]
 )
