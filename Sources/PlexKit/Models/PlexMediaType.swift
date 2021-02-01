4a13b49574434d35052fa53474434c7a80b1ba61
Lachlan Charlick <lachlan.charlick@gmail.com>
2021-02-01 16:09:36 +1030

    Resilient `PlexMediaType` decoding (fixes #7)

--- Sources/PlexKit/Models/PlexMediaType.swift
+++ Sources/PlexKit/Models/PlexMediaType.swift
@@ -9,7 +9,9 @@
 import Foundation
 
 /// - Note: https://github.com/Arcanemagus/plex-api/wiki/MediaTypes
-public enum PlexMediaType: String, Codable {
+public enum PlexMediaType: Hashable {
+    case unknown(String)
+
     case movie
     case show
     case season
@@ -30,11 +32,131 @@ public enum PlexMediaType: String, Codable {
     case userPlaylistItem
 
     case collection
+
+    init(rawValue: String) {
+        switch RawValue(rawValue: rawValue) {
+        case .movie:
+            self = .movie
+        case .show:
+            self = .show
+        case .season:
+            self = .season
+        case .episode:
+            self = .episode
+        case .trailer:
+            self = .trailer
+        case .comic:
+            self = .comic
+        case .person:
+            self = .person
+        case .artist:
+            self = .artist
+        case .album:
+            self = .album
+        case .track:
+            self = .track
+        case .photoAlbum:
+            self = .photoAlbum
+        case .picture:
+            self = .picture
+        case .photo:
+            self = .photo
+        case .playlist:
+            self = .playlist
+        case .playlistFolder:
+            self = .playlistFolder
+        case .userPlaylistItem:
+            self = .userPlaylistItem
+        case .collection:
+            self = .collection
+        default:
+            self = .unknown(rawValue)
+        }
+    }
+
+    public var rawValue: String {
+        switch self {
+        case .unknown(let rawValue):
+            return rawValue
+        case .movie:
+            return RawValue.movie.rawValue
+        case .show:
+            return RawValue.show.rawValue
+        case .season:
+            return RawValue.season.rawValue
+        case .episode:
+            return RawValue.episode.rawValue
+        case .trailer:
+            return RawValue.trailer.rawValue
+        case .comic:
+            return RawValue.comic.rawValue
+        case .person:
+            return RawValue.person.rawValue
+        case .artist:
+            return RawValue.artist.rawValue
+        case .album:
+            return RawValue.album.rawValue
+        case .track:
+            return RawValue.track.rawValue
+        case .photoAlbum:
+            return RawValue.photoAlbum.rawValue
+        case .picture:
+            return RawValue.picture.rawValue
+        case .photo:
+            return RawValue.photo.rawValue
+        case .playlist:
+            return RawValue.playlist.rawValue
+        case .playlistFolder:
+            return RawValue.playlistFolder.rawValue
+        case .userPlaylistItem:
+            return RawValue.userPlaylistItem.rawValue
+        case .collection:
+            return RawValue.collection.rawValue
+        }
+    }
+
+    enum RawValue: String, Codable, CaseIterable {
+        case movie
+        case show
+        case season
+        case episode
+        case trailer
+        case comic
+        case person
+
+        case artist
+        case album
+        case track
+        case photoAlbum
+        case picture
+        case photo
+
+        case playlist
+        case playlistFolder
+        case userPlaylistItem
+
+        case collection
+    }
+}
+
+extension PlexMediaType: Codable {
+    public init(from decoder: Decoder) throws {
+        let rawValue = try decoder.singleValueContainer().decode(String.self)
+        self.init(rawValue: rawValue)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(rawValue)
+    }
 }
 
 extension PlexMediaType {
     var key: Int {
         switch self {
+        case .unknown:
+            return -1
+
         case .movie:
             return 1
         case .show:
---
+++ Tests/PlexKitTests/PlexMediaTypeTests.swift
@@ -0,0 +1,52 @@
+//
+//  PlexMediaTypeTests.swift
+//  PlexKitTests
+//
+//  Created by Lachlan Charlick on 1/2/21.
+//  Copyright © 2021 Lachlan Charlick. All rights reserved.
+//
+
+import XCTest
+@testable import PlexKit
+
+class PlexMediaTypeTests: XCTestCase {
+    func testInitFromRawValue() {
+        for value in PlexMediaType.RawValue.allCases {
+            let mediaType = PlexMediaType(rawValue: value.rawValue)
+            switch mediaType {
+            case .unknown:
+                XCTFail("Unexpected `unknown` media type!")
+            default:
+                break
+            }
+            XCTAssertEqual(mediaType.rawValue, value.rawValue)
+        }
+    }
+
+    func testDecoding() throws {
+        let decoder = JSONDecoder()
+
+        for value in PlexMediaType.RawValue.allCases {
+            let mediaType = PlexMediaType(rawValue: value.rawValue)
+
+            XCTAssertEqual(
+                try decoder.decode(Container.self, from: """
+                {"mediaType": "\(value.rawValue)"}
+                """.data(using: .utf8)!).mediaType,
+                mediaType
+            )
+        }
+
+        XCTAssertEqual(
+            try decoder.decode(Container.self, from: """
+            {"mediaType": "hello"}
+            """.data(using: .utf8)!).mediaType,
+            .unknown("hello")
+        )
+
+    }
+}
+
+private struct Container: Codable {
+    let mediaType: PlexMediaType
+}
