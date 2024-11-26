29484d1922055dd85130998b0ef2eea9704b5503
Lachlan Charlick <lachlan.charlick@gmail.com>
2024-11-26 21:33:25 +1030

    Decode collection `childCount` as an Int
    Fixes prologueapp/Prologue#74

--- Sources/PlexKit/Models/PlexCollection.swift
+++ Sources/PlexKit/Models/PlexCollection.swift
@@ -23,10 +23,61 @@ public struct PlexCollection: Codable, Hashable {
     public let thumb: String?
     public let addedAt: Date?
     public let updatedAt: Date?
-    public let childCount: String?
+    public let childCount: Int?
     public let maxYear: String?
     public let minYear: String?
     private let smart: String?
 
     public var isSmart: Bool { smart == "1" }
+
+    private enum CodingKeys: String, CodingKey {
+        case key
+        case title
+        case titleSort
+        case ratingKey
+        case guid
+        case type
+        case subtype
+        case summary
+        case index
+        case ratingCount
+        case thumb
+        case addedAt
+        case updatedAt
+        case childCount
+        case maxYear
+        case minYear
+        case smart
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        key = try container.decode(String.self, forKey: .key)
+        title = try container.decode(String.self, forKey: .title)
+
+        titleSort = try container.decodeIfPresent(String.self, forKey: .titleSort)
+        ratingKey = try container.decodeIfPresent(String.self, forKey: .ratingKey)
+        guid = try container.decodeIfPresent(String.self, forKey: .guid)
+        type = try container.decodeIfPresent(String.self, forKey: .type)
+        subtype = try container.decodeIfPresent(PlexMediaType.self, forKey: .subtype)
+        summary = try container.decodeIfPresent(String.self, forKey: .summary)
+        index = try container.decodeIfPresent(Int.self, forKey: .index)
+        ratingCount = try container.decodeIfPresent(Int.self, forKey: .ratingCount)
+        thumb = try container.decodeIfPresent(String.self, forKey: .thumb)
+        addedAt = try container.decodeIfPresent(Date.self, forKey: .addedAt)
+        updatedAt = try container.decodeIfPresent(Date.self, forKey: .updatedAt)
+        maxYear = try container.decodeIfPresent(String.self, forKey: .maxYear)
+        minYear = try container.decodeIfPresent(String.self, forKey: .minYear)
+        smart = try container.decodeIfPresent(String.self, forKey: .smart)
+
+        // As of PMS 1.41.2, `childCount` is an int, not a string.
+        if let intValue = try? container.decode(Int.self, forKey: .childCount) {
+            childCount = intValue
+        } else if let stringValue = try? container.decode(String.self, forKey: .childCount) {
+            childCount = Int(stringValue)
+        } else {
+            childCount = nil
+        }
+    }
 }
